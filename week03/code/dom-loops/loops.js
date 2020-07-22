5a96c61715d779483401bb853503c2338e1652c2
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-07-21 20:01:29 -0500

    we were looping

---
+++ week03/code/dom-loops/loops.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const colors = ['red', 'orange', 'blue', 'yellow', 'green']
+
+const colorLoop = () => {
+    let domString = '';
+
+    for(let i = 0; i < colors.length; i++) {
+        domString += `<h1>${colors[i]}</h1>`
+    }
+
+    console.log(domString)
+}
+
+colorLoop()
